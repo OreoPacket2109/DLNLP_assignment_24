53ea6b6a8f56c646f770ff9af16302df6a3b1dbc
OreoPacket2109 <150846859+OreoPacket2109@users.noreply.github.com>
2024-04-08 22:53:40 +0100

    Imported datasets

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hi")
+#Importing dependencies
+import numpy as np
+import pandas as pd
+
+#====================|1. Importing Datasets|====================
+
+#Importing first dataset
+dir_a = "Datasets/labeled_data.csv"
+df_a = pd.read_csv(dir_a)
+dataset_a = df_a['tweet']
+labels_a = df_a['class']
+
+#Importing second dataset
+dir_b = "Datasets/additional_data.csv"
+df_b = pd.read_csv(dir_b)
+dataset_b = df_b[df_b['label'] == 'hate']
+dataset_b = dataset_b['text']
+labels_b = pd.Series(np.zeros(len(dataset_b)))
+dataset_b.reset_index(drop = True, inplace = True)
+
+#Combining the two datasets
+combined_dataset = pd.concat([dataset_a, dataset_b], ignore_index=True)
+labels_b.reset_index(drop = True, inplace = True)
+combined_labels = pd.concat([labels_a, labels_b], ignore_index=True)
+
+#====================|2. Dataset Analysis|====================
